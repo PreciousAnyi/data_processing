49ac6111bb1fd96efae0d664ffabffc7ddf712fe
PreciousAnyi <jkpresh@gmail.com>
2023-05-10 06:23:25 -0700

    first commit

---
+++ totals.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  if (args.isEmpty) {
+    print("Usage: dart totals.dart <inputFile.csv>");
+    exit(1);
+  } else {
+    final inputFile = args.first;
+    final lines = File(inputFile).readAsLinesSync();
+    for (var line in lines) {
+      print(line);
+    }
+  }
+}
